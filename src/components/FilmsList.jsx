a2f7d3c74a15c6c89ada27b9e4e7b7811cfa4fe7
MauroBorzi <mauroborzi91@gmail.com>
2025-09-01 14:45:51 +0200

    [add]select

--- src/components/FilmsList.jsx
+++ src/components/FilmsList.jsx
@@ -11,6 +11,15 @@ const FilmsList = () => {
   return (
     <div className="container">
       <div className="row">
+        <div className="col-12 mt-3">
+          <select className="form-select" aria-label="Default select example">
+            <option selected>Seleziona categoria film</option>
+            <option value="Fantascienza">Fantascienza</option>
+            <option value="Thriller">Thriller</option>
+            <option value="Romantico">Romantico</option>
+            <option value="Azione">Azione</option>
+          </select>
+        </div>
         <div className="col-12 mt-5">
           <ol className="list-group list-group-numbered">
             {filmsList.map((film, index) => {
