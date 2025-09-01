9e5655a953b47928c4b00799dc6b617bb8dcfc09
MauroBorzi <mauroborzi91@gmail.com>
2025-09-01 15:15:14 +0200

    [add]filter

--- src/components/FilmsList.jsx
+++ src/components/FilmsList.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react"
+
 const filmsList = [
   { title: 'Inception', genre: 'Fantascienza' },
   { title: 'Il Padrino', genre: 'Thriller' },
@@ -8,11 +10,22 @@ const filmsList = [
 ]
 
 const FilmsList = () => {
+  const [films, setFilms] = useState(filmsList)
+  const [search, setSearch] = useState("")
+  const [filteredFilms, setFilteredFilms] = useState(films)
+
+  useEffect(() => {
+    const filteredFilm = films.filter(film => {
+      return film.genre.toLowerCase().includes(search.toLowerCase())
+    })
+    setFilteredFilms(filteredFilm)
+  }, [search])
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-12 mt-3">
-          <select className="form-select" aria-label="Default select example">
+          <select className="form-select" aria-label="Select films" value={search} onChange={e => setSearch(e.target.value)}>
             <option selected>Seleziona categoria film</option>
             <option value="Fantascienza">Fantascienza</option>
             <option value="Thriller">Thriller</option>
@@ -22,7 +35,7 @@ const FilmsList = () => {
         </div>
         <div className="col-12 mt-5">
           <ol className="list-group list-group-numbered">
-            {filmsList.map((film, index) => {
+            {filteredFilms.map((film, index) => {
               return (
                 <li className="list-group-item" key={index}>
                   {film.title}
